c49713a2982885e59e3b78ae53a156b1a6572a9d
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-05-14 18:54:27 +0200

    added the book function in the index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("HELLO START HERE");
+import { TITLE, AUTHOR, PAGES, READ } from "./dom.js";
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+function render() {
+  return myLibrary;
+}
+console.log(myLibrary);
