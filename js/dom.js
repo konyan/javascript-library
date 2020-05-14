d626b75bcd4166efb2f6cc602cdfd99ab46ceb2b
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-05-14 19:19:48 +0200

    created  the add to book function

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,3 @@
-let TITLE = document.getElementById("title").value;
-let AUTHOR = document.getElementById("author").value;
-let PAGES = document.getElementById("pages").value;
-let READ = document.getElementById("read").value;
+
 
 export const {TITLE,AUTHOR,PAGES,READ}
--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
-import { TITLE, AUTHOR, PAGES, READ } from "./dom.js";
-
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
+function Book(title, author, pages) {
+  (this.title = title), (this.author = author), (this.pages = pages);
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+function addBookToLibrary() {
+  const myBook = new Book(title, author, pages);
+
+  myLibrary.push(myBook);
 }
+
+addNewForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const author = document.getElementById("author-input").value;
+  const title = document.getElementById("title").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked;
+  addBookToLibrary(logic.newBook(author, title, pages, read));
+  //   clearForm();
+  //   render(logic.library);
+});
 
 function render() {
   return myLibrary;
